baedf5b4ae298b4893164534d5c4683871ef7957
ashwinishetty11 <95746221+ashwinishetty11@users.noreply.github.com>
2022-02-01 18:31:18 +0530

    Update fct_page_visit_daily.sql

--- ashwini-files/web-data-foundation/fct_page_visit_daily.sql
+++ ashwini-files/web-data-foundation/fct_page_visit_daily.sql
@@ -1,4 +1,4 @@
---insert into fct_page_visit_daily () 
+--daily load for page visits table
 
 with pages as
 (     select id page_visit_id 
@@ -14,20 +14,18 @@ with pages as
         , row_number() over (partition by anonymous_id,s_id order by timestamp desc) rnk2_isexit
 
         , context_locale language
-        , SUBSTR(context_locale, 0, 2) visit_ip_country -- need to recheck
         , tracking_utm_campaign utm_campaign
         , tracking_utm_source utm_source
         , tracking_utm_medium utm_medium
         , tracking_utm_term utm_term
-from `datascience-222717.confluentio_segment_prod.pages` pages
--- where timestamp >= DATE_ADD(DATE current_date, INTERVAL -2 DAY) and timestamp <=  DATE_ADD(DATE current_date, INTERVAL -1 DAY)  -- how to get the extract_date
+      from `datascience-222717.confluentio_segment_prod.pages` pages
+      where DATE(timestamp) = DATE('{{ next_execution_date.isoformat() }}') 
 )
 select page_visit_id 
         , page_url 
         , visitor_id 
         , cloud_user_id 
-        , session_id 
-        , visit_ip_country 
+        , session_id  
         , language 
         , visit_start_time
         , visit_end_time
@@ -38,7 +36,6 @@ select page_visit_id
         , (case when rnk1_isentrance = 1 then 'True' else 'False' end) is_entrance
         , (case when rnk2_isexit = 1     then 'True' else 'False' end) is_exit
         , (case when  rnk1_isentrance = 1 and rnk2_isexit = 1 then 'True' else 'False' end) is_bounce  
-  --      , DATE_ADD(DATE current_date, INTERVAL -2 DAY)  extract_date
+  --    ,  TIMESTAMP('{{next_execution_date.isoformat()}}') as extract_date
 from pages
-
 
