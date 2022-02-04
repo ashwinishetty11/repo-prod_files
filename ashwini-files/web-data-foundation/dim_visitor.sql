f004c191b296f72cb440b6f7e76a61dabbf30ab5
ashwinishetty11 <95746221+ashwinishetty11@users.noreply.github.com>
2022-02-04 20:49:07 +0530

    Update dim_visitor.sql

--- ashwini-files/web-data-foundation/dim_visitor.sql
+++ ashwini-files/web-data-foundation/dim_visitor.sql
@@ -1,9 +1,7 @@
 CREATE OR REPLACE TABLE dwh_web.dim_visitor as
 
-select anonymous_id visitor_id
-        , user_id cloud_user_id
-      --  , context_locale --country
+select anonymous_id visitor_id 
         , min(timestamp) first_visit_time
         , max(timestamp) most_recent_visit_time
 from confluentio_segment_prod.pages i
-group by  1,2
+group by  1
