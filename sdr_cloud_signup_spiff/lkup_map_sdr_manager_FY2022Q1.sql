3e03fcfb95a4fbd718f8259673a8bfed8b9c9665
ashwinishetty11 <95746221+ashwinishetty11@users.noreply.github.com>
2022-02-16 23:24:44 +0530

    Create lkup_map_sdr_manager_FY2022Q1.sql

---
+++ sdr_cloud_signup_spiff/lkup_map_sdr_manager_FY2022Q1.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE EXTERNAL TABLE `data-sandbox-123.Workspace_Ashwini.lkup_map_sdr_manager_FY2022Q1`  
+(
+  sdr_name	STRING,
+  manager_name	STRING
+)
+OPTIONS (
+  format = 'GOOGLE_SHEETS',
+  uris = ['https://docs.google.com/spreadsheets/d/11s_o0ePO-HvJo8DPc5AmGrbGTscrM3A4fo7YVlN0RVo/edit#gid=0'],
+  skip_leading_rows = 1
+);
