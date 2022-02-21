1730d4bc3827330b41ccc49a2c805220e63b9e45
ashwinishetty11 <95746221+ashwinishetty11@users.noreply.github.com>
2022-02-21 18:04:37 +0530

    Update rpt_sdr_assisted_signups.sql

--- sdr_cloud_signup_spiff/rpt_sdr_assisted_signups.sql
+++ sdr_cloud_signup_spiff/rpt_sdr_assisted_signups.sql
@@ -1,6 +1,6 @@
 CREATE OR REAPLCE sdr_cloud_signup_spiff.rpt_sdr_assisted_signups
 
-with lkup_manager_quarter_wise_grp_size as (
+with lkup_manager_quarterly_grp_size as (
   select manager_name
         , count(distinct sdr_name) grp_size
   from sdr_cloud_signup_spiff.lkup_sdr_manager
@@ -14,6 +14,6 @@ select a.*
         , grp_size.grp_size lkup_manager_grp_size
 from sdr_cloud_signup_spiff.sdr_assisted_signups a
     left join sdr_cloud_signup_spiff.lkup_sdr_manager lkup on a.ownerid = lkup.sdr_id
-    left join lkup_manager_quarter_wise_grp_size grp_size  on grp_size.manager_name = lkup.manager_name
+    left join lkup_manager_quarterly_grp_size grp_size  on grp_size.manager_name = lkup.manager_name
 
  
